764bcbaa496e8c32d3f6b86bf6a8c760dbba6175
Nana <juniornyamile@gmail.com>
2022-02-15 21:25:59 +0000

    1.3

--- src/App.js
+++ src/App.js
@@ -9,16 +9,15 @@ import Map from './components/Map/Map';
 
 const App = () => {
     return (
-
         <>
             <CssBaseline/>
-            <header />
+            <Header />
             <Grid container spacing = {3} style = {{width: '100%'}}>
                 <Grid item xs={12} md={4}>
                     <List />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <map />
+                    <Map />
                 </Grid>
             </Grid>
         </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
                 <Typography variant="h5" className={classes.title}>
                     Travel Advisor
                 </Typography>
-                <Box display={flex}>
+                <Box display="flex">
                     <Typography variant="h6" className={classes.title}>
                         Explore new places
                     </Typography>
