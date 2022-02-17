1fcdc535cae72db1892568b37fa1e8f7068a8237
Nana <juniornyamile@gmail.com>
2022-02-17 16:53:36 +0000

    list 1.1

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, select } from '@material-ui/core';
 
 
 import useStyles from './styles';
 
 const List = () => {
+  const classes = useStyles()
+  const [type, setType] = useState("restaurants");
+
   return (
-    <h1>List </h1>
+    <div className={classes.container}>
+      <Typography variant="h4"> Resturants, Hotels & Attractions around You</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </select>
+      </FormControl>
+    </div>
   );
 }
 
