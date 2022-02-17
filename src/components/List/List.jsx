619f7cfee6489e60eddffeb1fc9851cf985178fb
Nana <juniornyamile@gmail.com>
2022-02-17 17:00:14 +0000

    list 1.2

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -7,6 +7,7 @@ import useStyles from './styles';
 const List = () => {
   const classes = useStyles()
   const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
 
   return (
     <div className={classes.container}>
@@ -20,11 +21,12 @@ const List = () => {
         </select>
       </FormControl>
       <FormControl className={classes.formControl}>
-        <InputLabel>Type</InputLabel>
-        <select value={type} onChange={(e) => setType(e.target.value)}>
-          <MenuItem value="restaurants">Restaurants</MenuItem>
-          <MenuItem value="hotels">Hotels</MenuItem>
-          <MenuItem value="attractions">Attractions</MenuItem>
+        <InputLabel>Rating</InputLabel>
+        <select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>ALl</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
         </select>
       </FormControl>
     </div>
