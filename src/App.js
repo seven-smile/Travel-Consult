65faab0a13dd8da82378eeab9912d27fd21adb39
Nana <juniornyamile@gmail.com>
2022-02-18 19:46:02 +0000

    app up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
-
+import { getPlacesData } from './api';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
