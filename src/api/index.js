efe6353733cecf64e561916f1f58b1077b6f5545
Nana <juniornyamile@gmail.com>
2022-02-19 14:07:07 +0000

    in-1

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,34 @@
 import axios from 'axios';
 
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+const options = {
+ 
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    restaurant_tagcategory_standalone: '10591',
+    restaurant_tagcategory: '10591',
+    limit: '30',
+    currency: 'USD',
+    open_now: 'false',
+    lunit: 'km',
+    lang: 'en_US'
+  },
+  headers: {
+    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+    'x-rapidapi-key': 'bb5deb85acmshbc8005000bdf8e2p136515jsne1103c53df0c'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
 const getPlacesData =  async () => {
     try {
         const response = await axios.get();
