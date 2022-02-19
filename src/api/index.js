61fa2acf6e036e6ed55649d7800c4bf3c3dfd1e6
Nana <juniornyamile@gmail.com>
2022-02-19 14:13:28 +0000

    in-2

--- src/api/index.js
+++ src/api/index.js
@@ -9,29 +9,17 @@ const options = {
     tr_latitude: '12.838442',
     bl_longitude: '109.095887',
     tr_longitude: '109.149359',
-    restaurant_tagcategory_standalone: '10591',
-    restaurant_tagcategory: '10591',
-    limit: '30',
-    currency: 'USD',
-    open_now: 'false',
-    lunit: 'km',
-    lang: 'en_US'
-  },
+     },
   headers: {
-    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-    'x-rapidapi-key': 'bb5deb85acmshbc8005000bdf8e2p136515jsne1103c53df0c'
+    'x-rapidapi-host': ' ',
+    'x-rapidapi-key': ''
   }
 };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
 
 const getPlacesData =  async () => {
     try {
-        const response = await axios.get();
+        const { data: { data  } } = await axios.get(URL, options);
 
 
     }   catch (error) {
