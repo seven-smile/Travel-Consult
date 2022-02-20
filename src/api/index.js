790c5f78487b9e49743697719038172fef0cd565
Nana <juniornyamile@gmail.com>
2022-02-20 13:20:10 +0000

    ind-li-ups

--- src/api/index.js
+++ src/api/index.js
@@ -6,10 +6,10 @@ export const getPlacesData =  async (sw, ne) => {
     try {
         const { data: { data  } } = await axios.get(URL, {
            params: {
-              bl_latitude: '11.847676',
-              tr_latitude: '12.838442',
-              bl_longitude: '109.095887',
-              tr_longitude: '109.149359',
+              bl_latitude: 'sw.lat',
+              tr_latitude: 'ne.lat',
+              bl_longitude: 'sw.lng',
+              tr_longitude: 'ne.lng',
               },
             headers: {
               'x-rapidapi-host': ' ',
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,23 +5,12 @@ import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 import useStyles from './styles';
 
-const List = () => {
+const List = ({places}) => {
   const classes = useStyles()
   const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState("");
 
-  const places = [
-    { name: 'Cool Place'},
-    { name: 'Best Beer'},
-    { name: 'Best Steaks'},{ name: 'Cool Place'},
-    { name: 'Best Beer'},
-    { name: 'Best Steaks'},{ name: 'Cool Place'},
-    { name: 'Best Beer'},
-    { name: 'Best Steaks'},{ name: 'Cool Place'},
-    { name: 'Best Beer'},
-    { name: 'Best Steaks'},
-  ];
-
+ 
   return (
     <div className={classes.container}>
       <Typography variant="h4"> Restaurants, Hotels & Attractions around You</Typography>
