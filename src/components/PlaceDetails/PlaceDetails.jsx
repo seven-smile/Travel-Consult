03dce9aa3e8b94f6554da862f9f473ddd606d8d0
Nana <juniornyamile@gmail.com>
2022-02-21 16:03:50 +0000

    pd-1.1

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { Box, Typography, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/Location';
+import PhoneIcon from '@material-ui/core/Phone';
+import { Rating } from "@material-ui/lab/Rating";
+
+import useStyles from './styles';
 
 const PlaceDetails = ({place}) => {
     console.log(place);
