cc8de83fc92cc64dbe4878817306c09416c37fba
Nana <juniornyamile@gmail.com>
2022-02-22 14:06:01 +0000

    over 1.6

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Autocomplete } from '@material-ui/lab';
+/*import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
@@ -18,18 +18,52 @@ const Header = () => {
           <Typography variant='h6' className={classes.title}>
               Explore New Places
           </Typography>
-          {/*<Autocomplete>*/}
-              <div className={classes.search}>
+          {/*<Autocomplete>*/
+              /*<div className={classes.search}>
                 <div className={classes.searchIcon}>
                   <SearchIcon />
                 </div>
-                <InputBase placeholder='Search ...' className={{root: classes.inputRoot, input: classes.inputinput}} />
+                <InputBase placeholder='Search ...' classes={{root: classes.inputRoot, input: classes.inputInput}} />
               </div>
-         {/* </Autocomplete> */}
+         {/* </Autocomplete> */
+        /*</Box>
+      </Toolbar>
+    </AppBar> 
+    
+  );
+}
+
+export default Header;*/
+
+import React from "react";
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+ 
+const Header = () => {
+  return (
+    <AppBar position='static'>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant='h5' className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant='h6' className={classes.title}>
+              Explore New Places
+          </Typography>
+          {/*<Autocomplete>*/}
+                <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                    <SearchIcon />
+                  </div>
+                  <InputBase placeholder='Search ...' classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                </div>
+          {/*</Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar> 
     
+
   );
 }
 
