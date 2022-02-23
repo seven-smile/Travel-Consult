f2043c63d770a7328c6da1add61cc601e7078b2c
Nana <juniornyamile@gmail.com>
2022-02-23 14:36:56 +0000

    404 corrected

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const App = () => {
     const [ bounds, setBounds ] = useState( null);
 
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longtitude} }) => {
-            setCoordinates({ lat: latitude, lng: longtitude });
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+            setCoordinates({ lat: latitude, lng: long itude });
         })
     }, []);
 
@@ -62,8 +62,14 @@ import Map from './components/Map/Map';
 const App = () => {
     const [ places, setPlaces] = useState([]);
 
-    const [ coordinates, setCoordinates ] = useState({lat: 0, lng: 0});
+    const [ coordinates, setCoordinates ] = useState({});
     const [ bounds, setBounds ] = useState( null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        })
+    }, []);
 
     useEffect(() => {
         console.log(coordinates, bounds);
