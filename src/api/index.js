c28cec3c06b3a276192bd0bababb91f0734e4aad
Nana <juniornyamile@gmail.com>
2022-02-25 10:59:23 +0000

    dp1

--- src/api/index.js
+++ src/api/index.js
@@ -30,24 +30,25 @@ import { Data } from '@react-google-maps/api';
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
 //ERROR BELOW( INSIDE THE ASYNC FUNCTION . IT IS (sw, ne)  IN THE VIDEO)
+export const getPlacesData =  async (sw, ne) => {
 export const getPlacesData =  async () => {
 //ERROR ABOVE( INSIDE THE ASYNC FUNCTION . IT IS (sw, ne)  IN THE VIDEO)
 
   try {
       const { data: { data  } } = await axios.get(URL, {
         params: {
-          // THE FIGURES BELOW SHOULD BE CHANGED TO SW.LAT, NE.LAT,ETC.
-          bl_latitude:'11.847676',
-          tr_latitude: '12.838442',
-          bl_longitude: '109.095887',
-          tr_longitude: '109.149359',
+          // // THE FIGURES BELOW SHOULD BE CHANGED TO SW.LAT, NE.LAT,ETC.
+          // bl_latitude:'11.847676',
+          // tr_latitude: '12.838442',
+          // bl_longitude: '109.095887',
+          // tr_longitude: '109.149359',
 
            // THE FIGURES ABOVE SHOULD BE CHANGED TO SW.LAT, NE.LAT,ETC.
 
-          // bl_latitude: sw.lat,
-          // tr_latitude: ne.lat,
-          // bl_longitude: sw.lng,
-          // tr_longitude: ne.lng,
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
          
         },
         headers: {
