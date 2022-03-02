1387502bd9554ff6f164ebf218d48fc966c849a4
Nana <juniornyamile@gmail.com>
2022-03-02 12:31:09 +0000

    t-1:48:17-ranking-check

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Map from './components/Map/Map';
 
 const App = () => {
     const [ places, setPlaces] = useState([]);
+    const[filteredPlaces,setFilteredPlaces] = useState([]);
+
     const [childClicked, setChildClicked] = useState(null)
 
     const [ coordinates, setCoordinates ] = useState({});
@@ -24,10 +26,17 @@ const App = () => {
     }, []);
 
     useEffect(() => {
+        const filteredPlaces = places.filter((place) => place.rating > rating);
+
+        setFilteredPlaces(filteredPlaces);
+    }, [rating]);
+
+    useEffect(() => {
         setIsLoading(true)
         getPlacesData(type, bounds.sw, bounds.ne)
             .then((data) => {
                 setPlaces(data);
+                setFilteredPlaces([])
                 setIsLoading(false);
             })
         }, [type,coordinates, bounds]);
@@ -39,7 +48,7 @@ const App = () => {
             <Grid container spacing = {3} style= {{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
                     <List 
-                        places = {places}
+                        places = {filteredPlaces.lenght ? filteredPlaces : places}
                         childClicked = {childClicked}
                         isLoading={isLoading}
                         type = {type}
@@ -54,7 +63,7 @@ const App = () => {
                         setCoordinates = {setCoordinates}
                         setBounds = {setBounds}
                         coordinates = { coordinates }
-                        places = {places}
+                        places = {filteredPlaces.lenght ? filteredPlaces :places}
                         setChildClicked={setChildClicked}   
                     />
 
