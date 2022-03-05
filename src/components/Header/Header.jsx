2856f83dfe794a7eadf21b3665ad748d8aba6116
Nana <juniornyamile@gmail.com>
2022-03-05 23:58:08 +0000

    khw

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,30 @@
+/*import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles';
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position='static'>
+      <Toolbar classNyhdk.dbdv>
+                  <SearchIcon />
+                </div>
+                <InputBase placeholder='Search ...' classes={{root: classes.inputRoot, input: classes.inputInput}} />
+              </div>
+         {/* </Autocomplete> */
+        /*</Box>
+      </Toolbar>
+    </AppBar> 
+    
+  );
+}
+
+export default Header;*/
+
 import React from "react";
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
