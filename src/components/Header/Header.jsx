e9992edba4b4fec9f009db7eaec460f205cd54ca
Nana <juniornyamile@gmail.com>
2022-03-06 00:00:21 +0000

    fkndf

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,3 @@
-/*import React from 'react';
-import { Autocomplete } from '@react-google-maps/api';
-import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
-
-import useStyles from './styles';
-
-const Header = () => {
-  const classes = useStyles();
-hkbf urjjj jrujr
-    </AppBar> 
-    
-  );
-}
-
-export default Header;*/
-
 import React from "react";
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
