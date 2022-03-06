1b1a1c8adc672ac646a99bf6481f7a2f36c71e5a
Nana <juniornyamile@gmail.com>
2022-03-06 23:06:07 +0000

    t:52

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,12 +5,20 @@ import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles'; 
  
-const Header = () => {
+const Header = ({setCoordinates}) => {
   const classes = useStyles();
   const [autoComplete, setAutoComplete] = useState(null);
   
   const onLoad = (autoComplete) => setAutoComplete(autoComplete);
+  
+  const onPlaceChanged = () => {
+    const lat = autoComplete.getPlace().geometry.location.lat();
+    const lng = autoComplete.getPlace().geometry.location.lng();
 
+    setCoordinates({lat, lngs})
+
+
+  }
   return (
     <AppBar position='static'>
       <Toolbar className={classes.toolbar}>
@@ -21,7 +29,7 @@ const Header = () => {
           <Typography variant='h6' className={classes.title}>
               Explore New Places
           </Typography>
-          <Autocomplete onLoad = {} onPlaceChanged={}>
+          <Autocomplete onLoad = {onLoad} onPlaceChanged={onPlaceChanged}>
                 <div className={classes.search}>
                   <div className={classes.searchIcon}>
                     <SearchIcon />
