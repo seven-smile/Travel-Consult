3198e720db83c9c7deb474d808f5271371df039a
Nana <juniornyamile@gmail.com>
2022-03-16 16:24:57 +0000

    fjd

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -9,7 +9,7 @@ import useStyles from './styles';
 const PlaceDetails = ({place, selected, refProp }) => {
     const classes = useStyles();
 
-    if (selected) refProp?.current ?. scrollIntoView({ behavior: "smooth", block: 'start'})
+    if(selected) refProp?.current ?. scrollIntoView({ behavior: "smooth", block: 'start'})
 
     return (
         <Card elevation={6}>
