d022594833a8ffc122a0f36ea941f61767ad2d32
Nana <juniornyamile@gmail.com>
2022-03-26 16:36:20 +0000

    hds

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect }  from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
-
 import { getPlacesData } from './api';
 import Header from './components/Header/Header';
 import List from './components/List/List';
