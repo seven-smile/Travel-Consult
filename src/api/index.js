f7dbbc3547a3c3e3c868c9f8bc568c8001ae50a3
Nana <juniornyamile@gmail.com>
2022-03-27 23:36:50 +0000

    jbd

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 
-export const getPlacesData =  async (type, sw, ne) => {
-  try {
-      const { data: { data  } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
-        params: {
-          bl_latitude: sw.lat,
-          tr_latitude: ne.lat,
-          bl_longitude: sw.lng,
-          tr_longitude: ne.lng,
+// export const getPlacesData =  async (type, sw, ne) => {
+//   try {
+//       const { data: { data  } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+//         params: {
+//           bl_latitude: sw.lat,
+//           tr_latitude: ne.lat,
+//           bl_longitude: sw.lng,
+//           tr_longitude: ne.lng,
          
-        },
-        headers: {
-          'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-          'x-rapidapi-key': '7fac198fc7msh3fefa83f3b473bap14f61ejsn0143a2d4deae'
-        }
-      });
+//         },
+//         headers: {
+//           'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+//           'x-rapidapi-key': '7fac198fc7msh3fefa83f3b473bap14f61ejsn0143a2d4deae'
+//         }
+//       });
         
-      return data;
-  } catch (error) {
-      console.log(error)
-  }
-}    
+//       return data;
+//   } catch (error) {
+//       console.log(error)
+//   }
+// }    
