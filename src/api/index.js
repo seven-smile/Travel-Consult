864f835028f59c0f67507afbf08e55fbf7a77abe
Nana <juniornyamile@gmail.com>
2022-03-31 20:48:34 +0000

    jgerw

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-// export const getPlacesData =  async (type, sw, ne) => {
-//   try {
+export const getPlacesData =  async (type, sw, ne) => {
+  try {
       const { data: { data  } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
         params: {
           bl_latitude: sw.lat,
