104eebb544ae20288f0d4890b05b9d7e1df660b6
Nana <juniornyamile@gmail.com>
2022-04-04 10:28:03 +0000

    hsdb

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,34 +1,34 @@
-// import React, {useState, useEffect, createRef} from "react";
-// import { CircularProgress, Grid, Typography, InputLabel, FormControl,Select, MenuItem  } from "@material-ui/core";
-// import PlaceDetails from '../PlaceDetails/PlaceDetails';
-// import useStyles from './styles';
+import React, {useState, useEffect, createRef} from "react";
+import { CircularProgress, Grid, Typography, InputLabel, FormControl,Select, MenuItem  } from "@material-ui/core";
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles';
 
-// const List = ({places, childClicked, isLoading, type, setType, rating, setRating }) => {
-//   const classes =  useStyles();
-//   const [elRefs, setElRefs] = useState([]);
+const List = ({places, childClicked, isLoading, type, setType, rating, setRating }) => {
+  const classes =  useStyles();
+  const [elRefs, setElRefs] = useState([]);
   
-//   useEffect(() => {
-//     const refs = Array(places?.length).fill().map((_, i) => elRefs[i] || createRef());
+  useEffect(() => {
+    const refs = Array(places?.length).fill().map((_, i) => elRefs[i] || createRef());
       
-//     setElRefs(refs);
-//   },[places]);
+    setElRefs(refs);
+  },[places]);
   
-//   return (
-//     <div className={classes.container}>
-//       <Typography variant="h4"> Restaurants, Hotels & Attractions around You</Typography>
-//       {isLoading? (
-//         <div className={classes.loading}>
-//           <CircularProgress size = '5rem' />
-//         </div>
-//       ) : (
-//         <>
-//       <FormControl className={classes.formControl}>
-//         <InputLabel> Type</InputLabel>
-//         <Select value={type} onChange={(e) => setType(e.target.value)}>
-//           <MenuItem value="restaurants">Restaurants</MenuItem>
-//           <MenuItem value="hotels">Hotels</MenuItem>
-//           <MenuItem value="attractions">Attractions</MenuItem>
-//         </Select>
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4"> Restaurants, Hotels & Attractions around You</Typography>
+      {isLoading? (
+        <div className={classes.loading}>
+          <CircularProgress size = '5rem' />
+        </div>
+      ) : (
+        <>
+      <FormControl className={classes.formControl}>
+        <InputLabel> Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
       </FormControl>
       <FormControl className={classes.formControl}>
         <InputLabel> Rating </InputLabel>
